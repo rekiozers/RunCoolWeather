229f42993049015b6ea95239d303c22d220c1928
Your Name <you@example.com>
2022-04-06 21:30:04 +0800

    天气软件

---
+++ Weather/app/src/main/java/com/example/runcoolweather/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.example.runcoolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+
+}
---
+++ Weather/app/src/main/java/com/example/runcoolweather/util/HelpUtil.java
@@ -0,0 +1,14 @@
+package com.example.runcoolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HelpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
